cfeb755ad90cd539736ccda9648d1904d0590f20
Guillermotrejo1 <maynardtrejo.yahoo.com>
2023-08-09 18:13:25 -0500

    error div

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ async function renderMovies(e) {
 }
 }
 
+function errorHTML (Search,Error) {
+  if(Search === null){
+  return `<div class="empty">
+  <figure class="empty__img--wrapper">
+      <img class="empty__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq3ytoVkdDuKo_iKa-pK_hWDXo37WACBjzcg&usqp=CAU" alt="" class="empty__img">
+  </figure>
+  <h1 class="help__txt">Search for movies ex. Title, Keywords</h1>
+  <h2 class="search__error--txt">${Search,Error}!</h2>
+</div>`
+}
+}
+
 
 function moviesHTML(movie) {
   return `<div class="movie">
